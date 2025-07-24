716b0ed60746666884bac18cd391f5caa3459ce6
Julia060290 <juliakalyhnevich@yandex.by>
2025-07-24 12:57:07 +0300

    Added sidebar links

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,10 +2,15 @@ import coins from "@assets/icons/coins.svg";
 import users from "@assets/icons/users.svg";
 import fingerprint from "@assets/icons/fingerprint.svg";
 import pricetag from "@assets/icons/pricetag.svg";
-import document from "@assets/icons/document.svg";
+import documentIcon from "@assets/icons/document.svg";
 import cog from "@assets/icons/cog.svg";
 import instruments from "@assets/icons/instruments.svg";
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import { createPortal } from "react-dom";
+import Popup from "@components/Popup/Popup.jsx";
+import TermsCondionsText from "@pages/Register/TermsCondionsText";
+import PrivacyPolicyText from "@pages/Register/PrivacyPolicyText";
 import "./Sidebar.css";
 
 const links = [
@@ -20,7 +25,7 @@ const links = [
     },
     {
         name: "Reserves and liquidity control",
-        icon: document,
+        icon: documentIcon,
         path: "#",
         disabled: true,
     },
@@ -32,8 +37,12 @@ const links = [
         disabled: false,
     },
 ];
+const modalsRoot = document.getElementById("modals-root");
 
 function Sidebar() {
+    const [showPrivacy, setShowPrivacy] = useState(false);
+    const [showTerms, setShowTerms] = useState(false);
+
     return (
         <aside className="sidebar">
             <nav>
@@ -67,6 +76,86 @@ function Sidebar() {
                     ))}
                 </ul>
             </nav>
+            <div className="sidebar-bottom">
+                <ul>
+                    <li>
+                        <a
+                            href="#"
+                            onClick={(e) => {
+                                e.preventDefault();
+                                setShowPrivacy(true);
+                            }}
+                        >
+                            Privacy Policy
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="#"
+                            onClick={(e) => {
+                                e.preventDefault();
+                                setShowTerms(true);
+                            }}
+                        >
+                            Terms and Conditions
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="https://github.com/OpenSourceIsFun"
+                            target="_blank"
+                        >
+                            Our GitHub
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            {showPrivacy &&
+                createPortal(
+                    <Popup
+                        header={<h2>Privacy Policy</h2>}
+                        body={<PrivacyPolicyText />}
+                        footer={
+                            <button
+                                className="button"
+                                onClick={() => {
+                                    setShowPrivacy(false);
+                                }}
+                            >
+                                Close
+                            </button>
+                        }
+                        closeHandler={() => {
+                            setShowPrivacy(false);
+                        }}
+                    />,
+                    modalsRoot,
+                    "privacy-policy"
+                )}
+
+            {showTerms &&
+                createPortal(
+                    <Popup
+                        header={<h2>Terms and Conditions</h2>}
+                        body={<TermsCondionsText />}
+                        footer={
+                            <button
+                                className="button"
+                                onClick={() => {
+                                    setShowTerms(false);
+                                }}
+                            >
+                                Close
+                            </button>
+                        }
+                        closeHandler={() => {
+                            setShowTerms(false);
+                        }}
+                    />,
+                    modalsRoot,
+                    "terms-conditions"
+                )}
         </aside>
     );
 }
